0fc42851209fa758261be445756fc830f6464087
M74B <Bigmolotochek@gmail.com>
2024-11-10 13:07:57 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,21 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception { // throws Exception по тому что может выбрасывать исключения
-        Scanner scanner = new Scanner(System.in); //Считывать с клавы
+    public static void main(String[] args) throws Exception { 
+        Scanner scanner = new Scanner(System.in); 
         String input = scanner.nextLine();
         System.out.println(calc(input));
     }
 
     public static String calc(String input) throws Exception {
-        String[] parts = input.split(""); // Разбивает введенную строку
+        String[] parts = input.split(" "); 
         int a = Integer.parseInt(parts[0]);
         String symbol = parts[1];
         int b = Integer.parseInt(parts[2]);
-        if (parts.length > 3) {// проверяет длинну
+        if (parts.length > 3) {
             throw new Exception("Формат математической операции не удовлетворяет заданию - два операнда и один операнд");
         }
-        if (a > 100 || b > 100 || a <= 0 || b <= 0) {
+        if (a > 10 || b > 10 || a <= 0 || b <= 0) {
             throw new Exception("Превышен лимит допустимых значений");
         }
         int c = 0;
@@ -27,7 +27,12 @@ public class Main {
             c = a * b;
         } else if (symbol.equals("/")) {
             c = a / b;
-        }// это считывает и выполняет мат операции
+        }
+
+        if (c < 0) {
+            throw new Exception ("Операция не может быть выполнена с отрицательными числами");
+        
+        }
         return String.valueOf(c);
 
     }
